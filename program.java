e34773d164886988224ac9b3ac0c4b13a8bc9ae0
revanth <atmakurirevanth@gmail.com>
2020-04-20 00:07:12 +0530

    Client program

--- program.java
+++ program.java
@@ -1,5 +1,11 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.UnknownHostException;
 class program{
    public static void main(String[] args){
-   
+      
    }
 }
